f9892076bedadcbedcbb36d4e85f828e1f8d2d84
Mohamed Dahir Osman <mohameddahirosman57@gmail.com>
2026-01-07 22:10:27 +0300

    Add files via upload

---
+++ Jan7_DataTypes.py
@@ -0,0 +1,28 @@
+# Jan 07 — Data Types Practice
+
+# 1. Create variables of all 4 types
+my_int = 10
+my_float = 20.5
+my_str = "Python"
+my_bool = True
+
+print(f"Integer: {type(my_int)}")
+print(f"Float: {type(my_float)}")
+print(f"String: {type(my_str)}")
+print(f"Boolean: {type(my_bool)}")
+
+# 2. Adding Boolean to Integer
+result = 10 + True
+print(f"10 + True = {result} (Type: {type(result)})")
+
+# 3. Check rating type
+rating = "4.5"
+print(f"Rating '{rating}' is of type: {type(rating)}")
+
+# --- Your Turn ---
+# 1. Create a variable 'sales' with value 1500.50
+# 2. Check its type.
+# 3. Create a boolean 'is_open' and set it to False.
+# 4. Check its type.
+
+# WRITE YOUR CODE BELOW
